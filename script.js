4953b05afa8d06571cfa915e1e323ecba8bd4ff1
sawaiz <github@sawaizsyed.com>
2020-10-18 18:10:42 +0200

    Added E1

---
+++ script.js
@@ -0,0 +1,118 @@
+var db = {
+    "voyage": [
+        {
+            "title": "Pilot",
+            "year": 2020,
+            "seekers": [
+                { "name": "Sawaiz", "age": "Early 90's", "location": "Berlin" },
+                { "name": "Alice", "age": "Late 80's", "location": "Atlanta" },
+                { "name": "Mai", "age": "Early 90's", "location": "Kyoto" },
+                { "name": "Terence", "age": "Late 80's", "location": "Tallahassee" },
+                { "name": "Kasia", "age": "Late 80's", "location": "Krakow" },
+                { "name": "Lauren", "age": "Early 90's", "location": "Santa" },
+                { "name": "Manja", "age": "Early 90's", "location": "Ljubljana" },
+                { "name": "Chris", "age": "Early 80's", "location": "Atlanta" },
+                { "name": "Linh", "age": "Late 90's", "location": "Hanoi" },
+                { "name": "Andrew", "age": "Early 90's", "location": "Atlanta" }
+            ],
+            "excursions": [
+                {
+                    "theme": "Fall",
+                    "flavorText": "Chill is in the air, and leaves litter the ground.",
+                    "startDate": "2020-10-11",
+                    "endDate": "2020-10-17",
+                    "selections":
+                        [
+                            {
+                                "voyager": "Kasia",
+                                "song": "Several Times I",
+                                "artist": "Pieter Nooten & Michael Brook",
+                                "album": "Sleeps with the Fishes",
+                                "year": "1987",
+                                "youtube": "https://youtu.be/WhEv2hTUvlc"
+                            },
+                            {
+
+                                "voyager": "Lauren",
+                                "song": "Blood",
+                                "artist": "The Middle East",
+                                "album": "The Recordings of the Middle East",
+                                "year": "2009",
+                                "youtube": "https://youtu.be/9FxfjJa_24g"
+                            },
+                            {
+                                "voyager": "Terence",
+                                "song": "Movies",
+                                "artist": "Weyes Blood",
+                                "album": "Titanic Rising",
+                                "year": "2019",
+                                "youtube": "https://youtu.be/xniRJsus8pk"
+                            },
+                            {
+                                "voyager": "Andrew",
+                                "song": "Scott Pilgrim v. My GPA",
+                                "artist": "Mom Jeans",
+                                "album": "Best Buds",
+                                "year": "2016",
+                                "youtube": "https://youtu.be/zOEuZfjv3jM"
+                            },
+                            {
+                                "voyager": "Sawaiz",
+                                "song": "Rangers",
+                                "artist": "A Fine Frenzy",
+                                "album": "One Cell in the Sea",
+                                "year": "2007",
+                                "youtube": "https://youtu.be/l4mCcQZ7Vjk"
+                            },
+                            {
+                                "voyager": "Alice",
+                                "song": "Naked as We Came",
+                                "artist": "Iron & Wine",
+                                "album": "Our Endless Numbered Days",
+                                "year": "2004",
+                                "youtube": "https://youtu.be/LbDtXmpi5r4"
+                            },
+                            {
+                                "voyager": "Chris",
+                                "song": "Question",
+                                "artist": "Old 97's",
+                                "album": "Satellite Rides",
+                                "year": "2001",
+                                "youtube": "https://youtu.be/_-5oLJkUSjA"
+                            },
+                            {
+                                "voyager": "Linh",
+                                "song": "September in the Rain",
+                                "artist": "Roy Hargrove",
+                                "album": "Approaching Standards",
+                                "year": "1994",
+                                "youtube": "https://youtu.be/iDmSVTH7Qq0"
+                            },
+                            {
+                                "voyager": "Mai",
+                                "song": "Samurai",
+                                "artist": "Jazztronik",
+                                "album": "Jazztronik",
+                                "year": "2005",
+                                "youtube": "https://youtu.be/LP0CS_epbcc"
+                            },
+                            {
+                                "voyager": "Manja",
+                                "song": "September",
+                                "artist": "Earth Wind & Fire",
+                                "album": "The Best of Earth, Wind & Fire, Vol. 1",
+                                "year": "1978",
+                                "youtube": "https://youtu.be/3cKtSlsYVEU"
+                            }
+                        ]
+                }
+            ]
+        }
+    ]
+}
+
+
+
+function createTable(data){
+    
+}
